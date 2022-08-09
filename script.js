6b12a1e1b31820628ceee3384db29fd0f44a3640
priscabebby <priscabebbyu@gmail.com>
2022-08-09 18:24:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function start(){
+	var coverGame = document.getElementById('cover');
+	var playGame = document.getElementById('game');
+	var character = document.getElementById("character");
+	if (playGame.style.display == 'none') {
+    coverGame.style.display = 'block';
+   }
+  else {
+    coverGame.style.display = 'none';
+    playGame.style.display = 'block'; 
+}
+}
+
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },400);
+}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
